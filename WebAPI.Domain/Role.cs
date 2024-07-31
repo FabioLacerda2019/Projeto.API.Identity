acd288d69500df5c40709de4d67b77759bc34ad5
Fabio Goncalves de Lacerda <fabio.lacerda@pcservice.com.br>
2024-07-31 05:48:50 -0300

    primeiro commit

---
+++ WebAPI.Domain/Role.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Domain
+{
+    public class Role : IdentityRole<int> 
+    {
+        public List<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ WebAPI.Repository/Context.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WebAPI.Domain;
+
+namespace WebAPI.Repository;
+
+public class Context : IdentityDbContext<User>
+{
+    public Context(DbContextOptions<Context> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+                 
+                userRole.HasOne(ur => ur.Role)
+                        .WitMany(r => r.UserRoles)
+                        .HasForeignKey(ur => ur.RoleId)
+                        .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                        .WitMany(r => r.UserRoles)
+                        .HasForeignKey(ur => ur.UserId)
+                        .IsRequired();
+            });
+
+            builder.Entity<Organizacao>(org =>
+                 {
+                     org.ToTable("Organizacoes");
+                     org.HasKey(x => x.Id);
+
+                     org.HasMany<User>()
+                     .WithOne()
+                     .HasForeignKey(x => x.OrgId)
+                     .IsRequired(false);
+                 }
+                 );
+        }
+    
+}
